70d4c8622d662728494b59a8327a7e5cfb597c39
David Mostoller <dmostoller@gmail.com>
2023-12-04 18:42:44 -0500

    fixed errors

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "David:",
+    streetAddress: "721 Mercy St",
+  };
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+      };
+  };
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; 
+    return obj;
+  };
+
+
+function deleteFromEmployeeByKey(obj, key) {
+    return {
+        ...obj[key]
+      };
+    delete obj.key;
+    return obj;
+  };
+
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj;
+
+  };
